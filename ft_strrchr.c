42a97bf38d765a2fc30e353c9929109888ea84bc
Damien Ferjul <dferjul@c1r4p5.42nice.fr>
2022-11-14 12:52:29 +0100

    libft

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,25 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dferjul <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/11 09:53:20 by dferjul           #+#    #+#             */
-/*   Updated: 2022/11/14 12:48:27 by dferjul          ###   ########.fr       */
+/*   Created: 2022/11/14 12:38:53 by dferjul           #+#    #+#             */
+/*   Updated: 2022/11/14 12:51:57 by dferjul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-char	*ft_strchr(const char *s, int c)
+#include <stdio.h>
+#include "libft.h"
+char    *ft_strrchr(const char *s, int c)
 {
-	int	i;
+	int i;
 
-	i = 0;
+	i = ft_strlen(s);
 	while(s[i])
-	{
+	{	
 		if(c == s[i])
 			return ((char *)s + i);
-		i++;
+		i--;
 	}
 	return (0);
+}
+
+int main(void)
+{
+	printf("%s", ft_strrchr("abcdefgh", 'e'));
 }
