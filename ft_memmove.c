7682f8b3ecb6381bcaa3163f29beeb0c2c30df2e
Damien Ferjul <dferjul@c2r1p1.42nice.fr>
2022-11-20 14:54:03 +0100

    libft

---
+++ ft_memmove.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dferjul <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/20 14:21:15 by dferjul           #+#    #+#             */
+/*   Updated: 2022/11/20 14:43:08 by dferjul          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+	unsigned int	i;
+
+	i = 0;
+	s = (unsigned char *) src;
+	d = (unsigned char *) dst;
+	if (s < d)
+	{
+		while (i--)
+			d[len] = s[len];
+	}
+	else
+		while (i < len)
+			d++ = s++;
+	return (dst);
+}
